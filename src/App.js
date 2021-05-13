d641360a381b541232a2a64ec99b20177389b951
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-05-13 03:10:43 +0100

    Update Level 1
    Add bootstrap grid

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import Card from "./component/Card";
 
 function App() {
   return (
-    <div className="App ">
-      <div className="mainContainer">
+    <div className="App container">
+      <div className="mainContainer row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
         {countriesAll.map((country, index) => {
           return <Card key={index} country={country} />;
         })}
--- src/component/Card.js
+++ src/component/Card.js
@@ -3,22 +3,33 @@ import "../App.css";
 
 function Card({ country }) {
   return (
-    <div className="card">
-      <img className="cardImage" src={country.flag} alt="flag" />
-      <div className="cardText">
-        <h4 style={{ }}>{country.name}</h4>
-        <p style={{ padding: 0, margin: 0, fontSize: "0.7em" }}>
-          <b>Population: </b>
-          {country.population}
-        </p>
-        <p style={{ padding: 0, margin: 0, fontSize: "0.7em" }}>
-          <b>Region: </b>
-          {country.region}
-        </p>
-        <p style={{ padding: 0, margin: 0, fontSize: "0.7em" }}>
-          <b>Capital: </b>
-          {country.capital}
-        </p>
+    <div className="col">
+      <div className="card h-100">
+        <img className="cardImage card-img-top" src={country.flag} alt="flag" />
+        <div className="cardText card-body">
+          <h4 className="card-title">{country.name}</h4>
+          <p
+            className="card-text"
+            style={{ padding: 0, margin: 0, fontSize: "0.7em" }}
+          >
+            <b>Population: </b>
+            {country.population}
+          </p>
+          <p
+            className="card-text"
+            style={{ padding: 0, margin: 0, fontSize: "0.7em" }}
+          >
+            <b>Region: </b>
+            {country.region}
+          </p>
+          <p
+            className="card-text"
+            style={{ padding: 0, margin: 0, fontSize: "0.7em" }}
+          >
+            <b>Capital: </b>
+            {country.capital}
+          </p>
+        </div>
       </div>
     </div>
   );
