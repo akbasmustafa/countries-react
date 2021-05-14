a8a32f172b83c69780ac7a05110fa0cd2295c302
Mustafa <30720101+akbasmustafa@users.noreply.github.com>
2021-05-14 04:03:32 +0100

    Done Level 3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import React, { useState } from "react";
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  const [selectedRegion, setSelectedRegion] = useState("-1");
+  const [regions, setRegions] = useState(createRegionArray());
 
   function handleSearch(e) {
     setSearchText(e.target.value);
@@ -14,8 +16,28 @@ function App() {
     let re = new RegExp(searchText, "i");
     const isCountry = re.test(country.name);
     const isCapital = re.test(country.capital);
+    const isSelectedRegion =
+      selectedRegion === "-1"
+        ? true
+        : country.region === regions[parseInt(selectedRegion)]
+        ? true
+        : false;
 
-    return isCapital || isCountry;
+    return (isCapital || isCountry) && isSelectedRegion;
+  }
+
+  function handleSelect(e) {
+    setSelectedRegion(e.target.value);
+  }
+
+  function createRegionArray() {
+    let arr = [];
+    countriesAll.forEach((country) => {
+      if (arr.indexOf(country.region) === -1) {
+        arr.push(country.region);
+      }
+    });
+    return arr;
   }
 
   return (
@@ -30,6 +52,21 @@ function App() {
             value={searchText}
             onChange={handleSearch}
           />
+        </div>
+        <div className="col c-md-6 rounded">
+          <select
+            className="form-select"
+            onChange={handleSelect}
+            value={selectedRegion}
+            aria-label="Default select example"
+          >
+            <option value="-1">Filter by Region</option>
+            {regions.map((region, index) => (
+              <option key={index} value={index}>
+                {region}
+              </option>
+            ))}
+          </select>
         </div>
       </div>
       <div className="mainContainer row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
